d8bcd20003559b1f61a8ca74baf72b7cddcc3e70
usamkaki <39077865+usamkaki@users.noreply.github.com>
2024-09-14 01:25:47 +0530

    Add files via upload

---
+++ shell1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+#create a folder
+mkdir demo
+
+# create a file
+cd demo
+touch demo1 demo2
+
---
+++ shell2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+####################################
+# Author: Usama Thihami Kaki
+# Date: 13/09/2024
+#
+# This script is for nodehealth check
+#
+#Version: v1
+#####################################
+
+set -x #debug mode
+set -e #exit the script when there is an error
+set -o Pipeline fail #It checks if there is any error in a command that includes pipe
+
+
+df -h #To view disk information 
+
+free -g # To view memory
+
+nproc #Number of CPUs
+
+ps -ef # To get the list all of the process running in a virtual machine
+
+#ps -ef | grep "python" ---> here grep is used to find the specific process
+
+date | echo "this" #here it will not print the date as data is system default command and it goes to stdin and pipe does not send the request to the next command if it goes to stdin and it works only in stdout
+
+ps -ef | grep "python" | awk -F " " '{print $2 $8}' #here awk command is used to get the list of specific instead of getting all the process details and $<number> refers to columns which is needed.
+
+#curl vs wget --> curl is used to view the stuffs without downloading it and wget downloads the file first
+
+# sudo find/ -name <filename> --> to find all the files in a VM
